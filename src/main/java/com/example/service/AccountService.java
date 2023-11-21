3d16c4fea3d1ee8eac0e756623bbbff5aca65533
Tim Atobatele <dev.atobatele@gmail.com>
2023-11-21 23:53:46 +0000

    just removed an unused dependency

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,7 +1,6 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
